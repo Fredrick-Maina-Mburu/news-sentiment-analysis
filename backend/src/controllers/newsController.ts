e8fe261678c2eeca4163bc4bcbc2aff05d3005fd
FredrickMburu <109967236+FredrickMburu@users.noreply.github.com>
2024-11-05 00:33:13 +0300

    fetching news from different apis

--- backend/src/controllers/newsController.ts
+++ backend/src/controllers/newsController.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from 'express';
+import { fetchAllNews, saveNewsToDB } from '../services/newsService';
 
-export const getNews = async (req: Request, res: Response) => {
-  res.json({ message: "News route is working!" });
+export const updateNews = async (req: Request, res: Response) => {
+  try {
+    const query = req.query.q || 'business';
+    const articles = await fetchAllNews(query as string);
+
+    await saveNewsToDB(articles);
+
+    res.status(200).json({ message: 'News articles updated successfully from multiple sources.' });
+  } catch (error) {
+    console.log('Error :', error)
+    res.status(500).json({ message: 'Error updating news articles' });
+  }
 };
